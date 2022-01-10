4c23ce5b46c45d9427e0f71639b27634e6b9a080
Sumukh.N <sumukh.n@sainsburys.co.uk>
2022-01-10 18:51:06 +0530

    unit test for business layer

--- src/test/java/com/junit/business/SomeBusinessImplTest.java
+++ src/test/java/com/junit/business/SomeBusinessImplTest.java
@@ -1,14 +1,19 @@
 package com.junit.business;
 
 import com.junit.data.SomeDataImpl;
+import com.junit.item.ItemModel;
+import com.junit.item.ItemRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SomeBusinessImplTest {
@@ -18,6 +23,9 @@ class SomeBusinessImplTest {
 
     @Mock
     SomeDataImpl someData;
+
+    @Mock
+    ItemRepository repository;
 
     @Test
     void calculateSum() {
@@ -42,5 +50,21 @@ class SomeBusinessImplTest {
     void calcuateSumfromData() {
         when(someData.getAllData()).thenReturn(new int[]{1,2,3});
         assertEquals(6,someBusiness.calcuateSumfromData());
+    }
+    @Test
+    void returndummyItemTest(){
+        ItemModel expected=new ItemModel(2,"Box",1000,3);
+        assertEquals(expected,someBusiness.returndummyItem());
+    }
+    @Test
+    public void findAllDataTest(){
+        when(repository.findAll()).thenReturn(Arrays.asList(
+                new ItemModel(1,"Fan",100,2),
+                new ItemModel(2,"Book",50,3)));
+        List<ItemModel> expected=Arrays.asList(
+                new ItemModel(1,"Fan",100,2,200),
+                new ItemModel(2,"Book",50,3,150));
+        assertEquals(expected,someBusiness.findAllData());
+        verify(repository,times(1)).findAll();
     }
 }
